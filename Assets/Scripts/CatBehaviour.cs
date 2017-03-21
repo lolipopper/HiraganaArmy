5212cf8879e41eae52e1e9aa8d0a26c098795021
Nathan Runtuwene <njruntuwene@gmail.com>
2017-03-22 08:47:25 +0900

    Prefabs

---
+++ Assets/Scripts/CatBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("ChangePosition", 0, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChangePosition() {
+		Vector3 curPosition = transform.position;
+		curPosition.x -= 0.5F;
+		transform.position = curPosition;
+	}
+}
---
+++ Assets/Scripts/FishBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBehaviour : MonoBehaviour {
+
+	bool selected = false;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("ChangePosition", 0, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selected && Input.GetKey (KeyCode.Space)) {
+			doAction ();
+		}
+	}
+
+	void OnMouseDown () {
+		selected = true;
+	}
+
+	void doAction() {
+		Debug.Log ("ACTION!");
+	}
+
+	void ChangePosition() {
+		Vector3 curPosition = transform.position;
+		curPosition.x += 0.5F;
+		transform.position = curPosition;
+	}
+}
